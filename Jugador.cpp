27e8aa2c9808ebb059d23c4a889543ccad6c58d1
TiffannyVarela <tiffanny_varela529@unitec.edu>
2019-08-23 13:58:31 -0600

    Jugador

---
+++ Jugador.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+#ifndef JUGADOR_CPP
+#define JUGADOR_CPP
+using namespace std;
+
+class Jugador{
+	protected:
+		string nombre, dorsal, pais_origen;
+		
+	public:
+		Jugador(){
+		}
+		
+		Jugador(string nombre, string dorsal, string pais_origen){
+			this->nombre=nombre;
+			this->dorsal=dorsal;
+			this->pais_origen=pais_origen;
+		}
+		
+		string getNombre(){
+			return nombre;
+		}
+		
+		void setNombre(string nombre){
+			this->nombre.assign(nombre);
+		}
+		
+		string getDorsal(){
+			return dorsal;
+		}
+		
+		void setDorsal(string dorsal){
+			this->dorsal.assign(dorsal);
+		}
+		
+		string getPais_origen(){
+			return pais_origen;
+		}
+		
+		void setPais_origen(string pais_origen){
+			this->pais_origen.assign(pais_origen);
+		}
+};
+
+#endif
